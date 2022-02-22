f485f338075ddce7a538b04645b3c16ad59d4d02
Penk13 <natanaelsteven109@gmail.com>
2022-02-22 22:45:08 +0700

    heroku database (postgresql)

--- base/settings/production.py
+++ base/settings/production.py
@@ -1,4 +1,10 @@
 from .base import *
+import dj_database_url
+
 
 DEBUG = False
 ALLOWED_HOSTS = ['django-starter-template.herokuapp.com']
+
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
